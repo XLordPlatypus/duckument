bbbd61882d20576a1ec8099e0880acc5c230ed87
Leandro Aepli <leandro.aepli@hotmail.com>
2024-10-28 19:38:24 +0100

    fix: object id error

--- src/view/components/markdown/Edit.tsx
+++ src/view/components/markdown/Edit.tsx
@@ -1,18 +1,18 @@
-import {updatePage} from "../../api/Api";
-import {useParams} from "react-router-dom";
+import { updatePage } from "../../api/Api";
+import { useParams } from "react-router-dom";
 
-function Edit({text, setText}) {
-    const pageId = useParams()
+function Edit({ text, setText }) {
+    const { pageId } = useParams();
     return (
         <>
             <textarea spellCheck={false} className="text-field" value={text} onChange={(e) => {
-                setText(e.target.value)
+                setText(e.target.value);
                 if (pageId) {
-                    updatePage(null, e.target.value, pageId.toString());
+                    updatePage(null, e.target.value, pageId);
                 }
             }}></textarea>
         </>
-    )
+    );
 }
 
 export default Edit;
